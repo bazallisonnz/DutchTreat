9a76644650b73aee15011b00268a19b88f8086b3
Barry Allison <bazallison@hotmail.com>
2017-11-21 18:09:47 +1300

    Finished Getting Started with JavaScript

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,15 +1,26 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function() {
+    var x = 0;
+    var s = "";
 
-console.log("Hello Pluralsight");
+    console.log("Hello Pluralsight");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function() {
-    console.log("Buying Item");
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
 });
-
-var productInfo = document.getElementsByClassName("product-props");
-var listItems = productInfo.item[0].chilren;
