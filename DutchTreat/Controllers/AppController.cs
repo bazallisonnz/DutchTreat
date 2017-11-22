22afaf0739363b187fbc164d74d25f9874ae3fd2
bazallisonnz <bazallison@hotmail.com>
2017-11-22 20:57:47 +1300

    At end of Using Tag Helpers

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -13,5 +13,18 @@ namespace DutchTreat.Controllers
         {
             return View();
         }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
     }
 }
